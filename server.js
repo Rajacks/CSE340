fb020113ae499bfcfb23aeda8d96832a339b882c
Raymund antrone <ray20101996@gmail.com>
2026-09-08 03:50:48 +0800

    Complete Week 2 database integration

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+import "dotenv/config";
 import express from "express";
+import { testConnection } from "./src/models/db.js";
+import { getAllOrganizations } from "./src/models/organizations.js";
+import { getAllProjects } from "./src/models/projects.js";
 
 // Define the application environment
 const nodeEnv = process.env.NODE_ENV?.toLowerCase() || "production";
@@ -20,13 +24,19 @@ app.get("/", (req, res) => {
 });
 
 // Organizations page
-app.get("/organizations", (req, res) => {
-  res.render("organizations", { title: "Organizations" });
+app.get("/organizations", async (req, res) => {
+  const organizations = await getAllOrganizations();
+  const title = "Our Partner Organizations";
+
+  res.render("organizations", { title, organizations });
 });
 
 // Projects page
-app.get("/projects", (req, res) => {
-  res.render("projects", { title: "Projects" });
+app.get("/projects", async (req, res) => {
+  const projects = await getAllProjects();
+  const title = "Service Projects";
+
+  res.render("projects", { title, projects });
 });
 
 // Categories page
@@ -34,7 +44,13 @@ app.get("/categories", (req, res) => {
   res.render("categories", { title: "Categories" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://127.0.0.1:${port}`);
-  console.log(`Environment: ${nodeEnv}`);
+app.listen(port, async () => {
+  try {
+    await testConnection();
+
+    console.log(`Server is running at http://127.0.0.1:${port}`);
+    console.log(`Environment: ${nodeEnv}`);
+  } catch (error) {
+    console.error("Unable to connect to the database.");
+  }
 });
